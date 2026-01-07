fee1009d34f91dac017385f920b6085da0a75a72
RafaPramudhita <rafapram360@gmail.com>
2026-01-08 05:08:59 +0700

    Create DestinasiHome navigation object
    - Define `DestinasiHome` implementing `DestinasiNavigasi`.
    - Set the route to "home" and use `app_name` as the title resource.

---
+++ app/src/main/java/com/example/contohfirebase/view/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.contohfirebase.view.route
+
+import com.example.contohfirebase.R
+
+object DestinasiHome: DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
