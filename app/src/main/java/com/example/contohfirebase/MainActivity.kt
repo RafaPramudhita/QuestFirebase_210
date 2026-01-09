ed85266585c9b54f1e12b935fe4786e8799af37f
RafaPramudhita <rafapram360@gmail.com>
2026-01-09 23:54:17 +0700

    Refactor MainActivity and add preview
    - Remove unnecessary `ContohfirebaseTheme` nesting.
    - Add `@Composable` and `@Preview` annotations for `Greeting` and `GreetingPreview`.
    - Add `Text` and `Modifier` imports.

--- app/src/main/java/com/example/contohfirebase/MainActivity.kt
+++ app/src/main/java/com/example/contohfirebase/MainActivity.kt
@@ -7,7 +7,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.contohfirebase.ui.theme.ContohfirebaseTheme
 import com.example.contohfirebase.view.controlNavigasi.DataSiswaApp
 
@@ -16,7 +19,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ContohfirebaseTheme {
+                ContohfirebaseTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     DataSiswaApp(
                         modifier = Modifier.padding(innerPadding)
